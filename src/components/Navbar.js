1c8795479c5e176277b61a79980fe03b390c21f2
Anna <you@example.com>
2022-10-19 00:17:27 +0200

    learning day 4 - css simple changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,13 +4,13 @@ import logo from "../images/logo.svg";
 export default function Navbar() {
     return (
         <div>
-            <div className="bg-black d-flex justify-content-space-between px-2">
+            <nav className="bg-black d-flex justify-content-space-between px-2">
                 <div className="d-flex">
                 <img src={logo} className="nav-logo"></img>
                 <h3 className="color-blue">ReactFacts</h3> 
                 </div>
                  <h4 className="sub-title">React Course - project 1</h4>
-            </div>
+            </nav>
            
         </div>
     );
